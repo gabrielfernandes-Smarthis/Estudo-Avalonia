4cd6a2ccb9d755f9d89e5c12bb9d9b9b9d49d461
Gabriel Gomes <ggfernandes30@gmail.com>
2023-08-29 11:49:06 -0300

    Adicionando MVVM ao projeto usando CommunityToolkit.MVVM

--- TesteAvalonia/ViewModels/MainViewModel.cs
+++ TesteAvalonia/ViewModels/MainViewModel.cs
@@ -1,7 +1,16 @@
-﻿namespace TesteAvalonia.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace TesteAvalonia.ViewModels;
+
+public partial class MainViewModel : ObservableObject
 {
-    public class MainViewModel : ViewModelBase
-    {
-        public string Greeting => "Welcome to Avalonia!";
-    }
+    [ObservableProperty]
+    private string tituloBold = "Avalonia";
+
+    [ObservableProperty]
+    private string tituloNormal = "LOUDNESS METER";
 }
--- TesteAvalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,8 +0,0 @@
-﻿using ReactiveUI;
-
-namespace TesteAvalonia.ViewModels
-{
-    public class ViewModelBase : ReactiveObject
-    {
-    }
-}
